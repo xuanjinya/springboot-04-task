ac2133ed9f6939509ea60cc387bc5f55de7875ff
xuanjinya <1067998193@qq.com>
2020-03-03 15:26:50 +0800

    Springboot任务学习

---
+++ src/main/java/com/example/task/Springboot04TaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot04TaskApplication.class, args);
+    }
+
+}
