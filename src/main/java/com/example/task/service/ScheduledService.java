5697d0ad614f3e8502cdeddd800aa386c198fb70
xuanjinya <1067998193@qq.com>
2020-03-03 17:32:56 +0800

    简单邮件

--- src/main/java/com/example/task/service/ScheduledService.java
+++ src/main/java/com/example/task/service/ScheduledService.java
@@ -15,8 +15,12 @@ public class ScheduledService {
     * second (秒),minute（分）, hour（时）, day of month（日）, month（月）, day of week（周几）
     * {@code "0 * * * * MON-FRI"} 【MON-FRI：周一到周五  * ：任何时间  0：整秒】 周一到周五，每一分钟都启动一次  0--》* 周一到周五，每一分钟每一秒都启动一次
      * */
-    @Scheduled(cron = "0 * * * * MON-SAT")
+//    @Scheduled(cron = "0 * * * * MON-SAT")
+//    @Scheduled(cron = "0,1,2,3,4,5 * * * * MON-SAT")
+//    @Scheduled(cron = "0-4 * * * * MON-SAT")
+    @Scheduled(cron = "0/4 * * * * MON-SAT") //每4秒执行一次
     public void hello(){
         System.out.println("hello ... ");
     }
+
 }
--- src/test/java/com/example/task/Springboot04TaskApplicationTests.java
+++ src/test/java/com/example/task/Springboot04TaskApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.task;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class Springboot04TaskApplicationTests {
+public class Springboot04TaskApplicationTests {
+
+
+    @Autowired
+    JavaMailSenderImpl mailSender;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test01(){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setSubject("通知");
+        simpleMailMessage.setText("今晚7：30开会");
+        simpleMailMessage.setTo("xuanjinya@gmail.com");
+        simpleMailMessage.setFrom("hbj1067998193@qq.com");
+        System.out.println(simpleMailMessage);
+        mailSender.send(simpleMailMessage);
     }
 
 }
