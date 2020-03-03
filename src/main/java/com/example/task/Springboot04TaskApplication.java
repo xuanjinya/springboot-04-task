7d337eeaed6df4405c45f920c775bbcdd05ba5b4
xuanjinya <1067998193@qq.com>
2020-03-03 15:50:12 +0800

    定时任务功能实现

--- src/main/java/com/example/task/Springboot04TaskApplication.java
+++ src/main/java/com/example/task/Springboot04TaskApplication.java
@@ -3,8 +3,9 @@ package com.example.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling //开启定时任务功能
 @EnableAsync //开启异步注解功能
 @SpringBootApplication
 public class Springboot04TaskApplication {
---
+++ src/main/java/com/example/task/service/ScheduledService.java
@@ -0,0 +1,22 @@
+package com.example.task.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author huang.beijin
+ * @date 2020/3/3 15:40
+ * @description
+ */
+@Service
+public class ScheduledService {
+
+    /*
+    * second (秒),minute（分）, hour（时）, day of month（日）, month（月）, day of week（周几）
+    * {@code "0 * * * * MON-FRI"} 【MON-FRI：周一到周五  * ：任何时间  0：整秒】 周一到周五，每一分钟都启动一次  0--》* 周一到周五，每一分钟每一秒都启动一次
+     * */
+    @Scheduled(cron = "0 * * * * MON-SAT")
+    public void hello(){
+        System.out.println("hello ... ");
+    }
+}
