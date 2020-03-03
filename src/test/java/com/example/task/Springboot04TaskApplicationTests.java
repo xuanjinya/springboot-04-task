c9c3d635c79978f632ea0d2af2c4b698607f8035
xuanjinya <1067998193@qq.com>
2020-03-03 17:44:41 +0800

    复杂邮件发送

--- src/test/java/com/example/task/Springboot04TaskApplicationTests.java
+++ src/test/java/com/example/task/Springboot04TaskApplicationTests.java
@@ -1,12 +1,19 @@
 package com.example.task;
 
+import com.sun.deploy.uitoolkit.ui.ModalityHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,8 +27,9 @@ public class Springboot04TaskApplicationTests {
     public void contextLoads() {
     }
 
+    //创建一个简单的邮件模板发送
     @Test
-    public void test01(){
+    public void test01() {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setSubject("通知");
         simpleMailMessage.setText("今晚7：30开会");
@@ -29,6 +37,22 @@ public class Springboot04TaskApplicationTests {
         simpleMailMessage.setFrom("hbj1067998193@qq.com");
         System.out.println(simpleMailMessage);
         mailSender.send(simpleMailMessage);
+    }
+
+    //创建一个复杂的邮件发送
+    @Test
+    public void test02() throws Exception {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setSubject("通知--今晚开会！");
+        mimeMessageHelper.setText("<b style='color:red'>今天7：30开会</b>",true);
+        mimeMessageHelper.setTo("xuanjinya@gmail.com");
+        mimeMessageHelper.setFrom("hbj1067998193@qq.com");
+
+        //上传文件
+        mimeMessageHelper.addAttachment("1.jpg", new File("H:\\资料\\照片头像\\炫金\\1.jpg"));
+        mimeMessageHelper.addAttachment("2.jpg", new File("H:\\资料\\照片头像\\炫金\\2.jpg"));
+        mailSender.send(mimeMessage);
     }
 
 }
